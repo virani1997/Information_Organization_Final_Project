09082e58d39b2f4fbc84a9b99a6925e89789f76e
Salman Virani <90786278+virani1997@users.noreply.github.com>
2022-05-17 22:39:16 +0500

    Add files via upload

---
+++ final.R
@@ -0,0 +1,23 @@
+library(readr)
+library(readxl)
+library(igraph)
+library(dplyr)
+
+citation <- read_csv("./data/citations.csv")
+paper <- read_xlsx("./data/papers.xlsx")
+
+paper_subset <- paper %>%
+  select(UT, Research_Areas)
+
+str(citation)
+
+str(paper)
+
+length(unique(paper$Research_Areas))
+length(unique(paper$UT))
+length(unique(citation$citing))
+length(unique(citation$cited))
+
+g1 <- graph_from_data_frame(d = citation, vertices = paper, 
+                            directed = FALSE)
+plot(g1)
